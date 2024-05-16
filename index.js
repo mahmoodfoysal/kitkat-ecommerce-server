c5ca31579dd7f0a2ec0f7fa5112fd43b146e256a
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-17 04:15:03 +0600

    vee

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 app.use(cors(
   {
     origin: [
-      // 'http://localhost:5173/',
+      'http://localhost:5173/',
       'ecommerce-vue3-compo-kitkat.web.app',
       'ecommerce-vue3-compo-kitkat.firebaseapp.com'
     ]

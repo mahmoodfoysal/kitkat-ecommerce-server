14013e06f5c66339004a8faad8d62cf7fd728c2d
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-17 14:01:22 +0600

    new access pass

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000;
-
+const app = express();
+const port = process.env.PORT;
 
 app.use(cors())
 app.use(express.json());

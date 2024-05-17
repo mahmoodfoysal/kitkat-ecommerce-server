15b169f8dbf816e2dff6fecdfe042698fe7aa461
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-17 12:28:31 +0600

    credential added

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 
-app.use(cors())
+app.use(cors(
+
+  {
+    origin: [
+      'http://localhost:5173/',
+      'ecommerce-vue3-compo-kitkat.web.app',
+      'ecommerce-vue3-compo-kitkat.firebaseapp.com'
+    ],
+    credentials: true
+  }
+))
 app.use(express.json());
 
 

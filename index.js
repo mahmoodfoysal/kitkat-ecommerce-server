f2b8d90cd9299c22226bdae8941e419515fc8f43
mahmoodfoysal <foysalcse033@gmail.com>
2024-05-17 20:20:12 +0600

    upload for render

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());

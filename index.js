9dd6ef9a60caf04ff049ee2f9b5106e453217496
Foysal Mahmood <foysalcse033@gmail.com>
2024-08-11 20:50:06 +0600

    admin get and post api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, Collection } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,7 +37,8 @@ async function run() {
     const brandCollection = database.collection("brands");
     const ordersCollection = database.collection("orders");
     const reviewCollection = database.collection("reviews");
-
+    // ******** Dashboard Collection **********
+    const adminCollection = database.collection('admin');
 
     // ########################## all post api are write here ###############################
     app.post('/squreCardForHome', async (req, res) => {
@@ -226,6 +227,39 @@ async function run() {
       }
     });
 
+    // *********************** Dashboard API ******************************* 
+    app.post('/admin', async (req, res) => {
+      const data = req.body;
+      try {
+        if (data._id) {
+          // Update operation
+          const userId = data._id;
+          delete data._id; // Remove _id from the userData to prevent overriding it
+    
+          const result = await adminCollection.updateOne(
+            { 
+              _id: new ObjectId(userId) 
+            },
+            { 
+              $set: data 
+            }
+          );
+    
+          if (result.matchedCount === 0) {
+            return res.status(404).send({ error: 'User not found' });
+          }
+          res.send(result);
+        } else {
+          // Create operation
+          const result = await adminCollection.insertOne(data);
+          res.status(201).send(result);
+        }
+      } catch (error) {
+        res.status(500).send({ error: 'Failed to create or update user' });
+      }
+    });
+
+
     // ####################### all get api are write here ######################### 
 
     app.get('/squreCardForHome', async(req, res) => {
@@ -261,7 +295,13 @@ async function run() {
       const getReviews = reviewCollection.find();
       const result = await getReviews.toArray();
       res.send(result);
-    })
+    });
+
+    app.get('/admin', async(req, res) => {
+      const getAdmin = adminCollection.find();
+      const result = await getAdmin.toArray();
+      res.send(result);
+    });
 
 
 
